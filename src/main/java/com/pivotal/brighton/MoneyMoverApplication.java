8b1de0a475426d3e86cfd92553c28ba724383bff
George Gelashvili, Priyan Parida and Ramadevi Nallamothu <pair+ggelashvili+priyanparida+RNallamothu@example.com>
2016-01-25 14:50:27 -0500

    changed to a maven-generated project structure instead of plain pom

---
+++ src/main/java/com/pivotal/brighton/MoneyMoverApplication.java
@@ -0,0 +1,12 @@
+package com.pivotal.brighton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMoverApplication.class, args);
+	}
+}
