cc3c38c84458f391929a03fb272b063af786c1c2
Priyan Parida <pair+priyanparida@example.com>
2016-01-28 15:14:08 -0500

    Added token retrieval to auth helper method
    Now gets it from request instead of expecting from caller

--- src/main/java/com/pivotal/brighton/AuthHelper.java
+++ src/main/java/com/pivotal/brighton/AuthHelper.java
@@ -4,9 +4,16 @@ import javax.servlet.http.HttpServletRequest;
 import java.math.BigInteger;
 
 public class AuthHelper {
-    public static boolean isAuthenticated(BigInteger token, HttpServletRequest httpServletRequest){
+    public static boolean isAuthenticated(HttpServletRequest httpServletRequest){
+        BigInteger token = getTokenFromRequest(httpServletRequest);
         BigInteger sessionToken = httpServletRequest.getSession().getAttribute("money-mover.user.authToken")!=null?
                 (BigInteger)httpServletRequest.getSession().getAttribute("money-mover.user.authToken"):null;
         return sessionToken.equals(token)?true:false;
+    }
+
+    private static BigInteger getTokenFromRequest(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getHeader("auth_token")!=null &&
+                    (Object)httpServletRequest.getHeader("auth_token") instanceof BigInteger?
+                    new BigInteger(httpServletRequest.getHeader("auth_token")):null;
     }
 }
