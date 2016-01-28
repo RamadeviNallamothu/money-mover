2921c8e4b032b07fbc840ec632f5432cdd9de112
George Gelashvili, Priyan Parida, Ramadevi Nallamothu and Sushanta kumar Dash <pair+ggelashvili+priyanparida+RNallamothu+Sushanta@example.com>
2016-01-28 16:26:21 -0500

    Auth token implemented in controller class

--- src/main/java/com/pivotal/brighton/controller/TransferController.java
+++ src/main/java/com/pivotal/brighton/controller/TransferController.java
@@ -1,12 +1,18 @@
 package com.pivotal.brighton.controller;
 
+import com.pivotal.brighton.AuthHelper;
+import com.pivotal.brighton.domain.AccountDetail;
 import com.pivotal.brighton.service.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.math.BigInteger;
 
 /**
  * Created by pivotal on 1/27/16.
@@ -19,10 +25,13 @@ public class TransferController {
 
     @RequestMapping(path = "/transfer", method = RequestMethod.POST)
     public ResponseEntity<String> moneyTransfer(@Param("sourceAccountID") String sourceAccountID, @Param("destinationAccountID") String destinationAccountID,
-                                                @Param("transactionAmount") double transactionAmount, @Param("transactionNotes") String transactionNotes )
+                                                @Param("transactionAmount") double transactionAmount, @Param("transactionNotes") String transactionNotes, HttpServletRequest httpServletRequest)
     {
+        if(AuthHelper.isAuthenticated(httpServletRequest))
+        {
+            return transferService.moneyTransfer(sourceAccountID,destinationAccountID, transactionAmount, transactionNotes);
+        }
 
-        return transferService.moneyTransfer(sourceAccountID,destinationAccountID, transactionAmount, transactionNotes);
-
+        return new ResponseEntity<String>("401", HttpStatus.UNAUTHORIZED);
     }
 }
