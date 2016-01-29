be163d381b88d32e4178b4968cdf4df38e2931f5
George Gelashvili and Priyan Parida <pair+ggelashvili+priyanparida@example.com>
2016-01-29 13:41:58 -0500

    Implement Json request handling in login
    Handling JSON post instead of form parameters

--- src/main/java/com/pivotal/brighton/controller/LoginController.java
+++ src/main/java/com/pivotal/brighton/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.pivotal.brighton.controller;
 
+import com.pivotal.brighton.dto.LoginRequest;
 import com.pivotal.brighton.dto.LoginResponse;
 import com.pivotal.brighton.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +20,10 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @RequestMapping(value="/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<LoginResponse> login(String username, String password, HttpServletRequest httpServletRequest) {
-        LoginResponse loginResponse = loginService.login(username, password, httpServletRequest);
+    @RequestMapping(value="/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest, HttpServletRequest httpServletRequest) {
+
+        LoginResponse loginResponse = loginService.login(loginRequest.getUsername(), loginRequest.getPassword(), httpServletRequest);
         return "SUCCESS".equals(loginResponse.getAuthResponse())?
                 new ResponseEntity<LoginResponse>(loginResponse, HttpStatus.ACCEPTED):
                 new ResponseEntity<LoginResponse>(loginResponse,HttpStatus.UNAUTHORIZED);
